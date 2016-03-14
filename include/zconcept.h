cf16044f9ca5d79e427fd9115f897a434df02f9d
zlsun <zlsun1995@gmail.com>
2016-03-14 21:24:39 +0800

    Remove useless code

--- include/zconcept.h
+++ include/zconcept.h
@@ -10,18 +10,6 @@
 
 NS_ZL_BEGIN
 
-namespace detail
-{
-    typedef char yes;
-    typedef char (&no)[2];
-    template <class T> yes check(T&&);
-    no check(...);
-}
-
-#define CONCEPT_DEFINE(name, expr) \
-    template <typename T>          \
-    constexpr bool name = sizeof(detail::check(expr)) == sizeof(detail::yes);
-
 #define CONCEPT_CHECK(C) std::enable_if_t<C>
 
 template <typename T>
--- tests/zconcept.cpp
+++ tests/zconcept.cpp
@@ -2,12 +2,22 @@
 #include "zconcept.h"
 using namespace zl;
 
-struct A {};
+struct A
+{
+};
+struct B
+{
+    friend std::ostream& operator << (std::ostream& os, const B&)
+    {
+        return os;
+    }
+};
 
 TEST(CanCout)
 {
     EQUAL(CanCout<int>, true)
     EQUAL(CanCout<A>, false)
+    EQUAL(CanCout<B>, true)
 }
 
 TEST_MAIN
