05c40d02aa7780fbb90f62820c98d9dae1831dca
zlsun <zlsun1995@gmail.com>
2016-04-24 11:08:22 +0800

    zproperty: better test

--- tests/zproperty.cpp
+++ tests/zproperty.cpp
@@ -3,27 +3,32 @@
 
 using namespace zl;
 
+auto getter = [](auto& prop) -> auto& {
+    ++prop.getAdd().gc;
+    return prop.getObj();
+};
+
+auto setter = [](auto& prop, const int& ni) {
+    ++prop.getAdd().sc;
+    prop.getObj() = ni;
+};
+
 class A
 {
 public:
     Property<int> i = 0;
-};
-
-class B
-{
-public:
     struct Counter { int gc = 0, sc = 0; };
-    Property<int, Counter> i {
-        0,
-        [](auto& prop) -> int& {
-            ++prop.getAdd().gc;
-            return prop.getObj();
-        },
-        [](auto& prop, const int& ni) {
-            ++prop.getAdd().sc;
-            prop.getObj() = ni;
-        }
-    };
+    Property<int, Counter> j {0, getter, setter};
+
+    // Test constuctor
+    Property<int, Counter> k1 {};
+    Property<int, Counter> k2 {0};
+    Property<int, Counter> k3 {getter};
+    Property<int, Counter> k4 {0, getter};
+    Property<int, Counter> k5 {setter};
+    Property<int, Counter> k6 {0, setter};
+    Property<int, Counter> k7 {getter, setter};
+    Property<int, Counter> k8 {0, getter, setter};
 };
 
 TEST(property_get)
@@ -31,11 +36,10 @@ TEST(property_get)
     A a;
     EQUAL(a.i.get(), 0)
     EQUAL(a.i, 0)
-    B b;
-    b.i.get();
-    EQUAL(b.i.getAdd().gc, 1)
-    b.i.get();
-    EQUAL(b.i.getAdd().gc, 2)
+    a.j.get();
+    EQUAL(a.j.getAdd().gc, 1)
+    a.j.get();
+    EQUAL(a.j.getAdd().gc, 2)
 }
 
 TEST(property_set)
@@ -45,11 +49,10 @@ TEST(property_set)
     EQUAL(a.i, 1)
     a.i = 2;
     EQUAL(a.i, 2)
-    B b;
-    b.i = 1;
-    EQUAL(b.i.getAdd().sc, 1)
-    b.i = 2;
-    EQUAL(b.i.getAdd().sc, 2)
+    a.j = 1;
+    EQUAL(a.j.getAdd().sc, 1)
+    a.j = 2;
+    EQUAL(a.j.getAdd().sc, 2)
 }
 
 TEST_MAIN
